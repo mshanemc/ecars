2973c2767103e04c779cd86f898c5d9537b84096
mshanemc <shane.mclaughlin@salesforce.com>
2020-11-02 14:15:50 -0600

    destroy last app

--- destroy.sh
+++ destroy.sh
@@ -2,3 +2,4 @@ sfdx shane:org:delete
 heroku destroy -a `basename "${PWD/mshanemc-/}"`-mqtt -c `basename "${PWD/mshanemc-/}"`-mqtt
 heroku destroy -a `basename "${PWD/mshanemc-/}"`-str -c `basename "${PWD/mshanemc-/}"`-str
 heroku destroy -a `basename "${PWD/mshanemc-/}"`-pwa -c `basename "${PWD/mshanemc-/}"`-pwa
+heroku destroy -a `basename "${PWD/mshanemc-/}"`-srv -c `basename "${PWD/mshanemc-/}"`-srv
